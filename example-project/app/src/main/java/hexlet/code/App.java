9f7f3f504c5182739115a38405dfbfb4bfaf058a
KateM <screpka.spb@mail.ru>
2022-11-09 01:31:34 +0300

    Step 4 done

--- example-project/app/src/main/java/hexlet/code/App.java
+++ example-project/app/src/main/java/hexlet/code/App.java
@@ -7,20 +7,28 @@ import picocli.CommandLine.Option;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        new CommandLine(new HelpDemo()).execute(args);
+        new CommandLine(new Help()).execute(args);
     }
 
 }
 
 @Command(mixinStandardHelpOptions = true, version = "help 1.0",
-        description = "Usage: gendiff [-hV]\nCompares two configuration files and shows a difference.")
-class HelpDemo implements Runnable {
+        description = """
+                Usage: gendiff [-hV] [-f=format] filepath1 filepath2
+                Compares two configuration files and shows a difference.
+                      filepath1         path to first file
+                      filepath2         path to second file""")
+class Help implements Runnable {
 
     @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
     boolean versionInfoRequested;
 
     @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
     boolean usageHelpRequested;
+
+    @Option(names = {"\b\b\b\b-f", "--format"}, //defaultValue = "stylish",
+            description = "\b\b=format   output format [default: stylish]")
+    boolean value;
 
     @Override
     public void run() {
